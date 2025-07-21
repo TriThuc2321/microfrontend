f7b379e65092936f9944fd381227e6070ea40b20
Thuc Tran <thuc.tran@chotot.vn>
2025-07-21 21:54:35 +0700

    chore: config webpack prod

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -3,23 +3,22 @@ const commonConfig = require("./webpack.common");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const packageJson = require("../package.json");
 
-const devConfig = {
-  mode: "development",
-  devServer: {
-    port: 8080,
-    historyApiFallback: {
-      index: "index.html",
-    },
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
   },
   plugins: [
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
   ],
 };
 
-module.exports = merge(commonConfig, devConfig);
+module.exports = merge(commonConfig, prodConfig);
